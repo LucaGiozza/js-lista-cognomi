497024df03c8e6f84f100ae308b87b9be8406b77
Luca Giozza <luca.giozza@gmail.com>
2021-05-31 21:12:14 +0200

    correzioni

--- js/script.js
+++ js/script.js
@@ -9,22 +9,24 @@
  for (var i = 0; i < nomi.length; i++){
     var risultato = "corretto, accesso disponibile"
     var negazione = " Mi dispiace, non sei in lista"
-
+ 
      if( mail == 'luca@gmail.com' ){
         document.getElementById("email").innerHTML =risultato;
         
-     } else if( mail == 'matteo@gmail.com'){
-        document.getElementById("email").innerHTML =risultato;
-     }else if( mail == 'simone@gmail.com'){
-        document.getElementById("email").innerHTML =risultato;
+      } else if( mail == 'matteo@gmail.com'){
+          document.getElementById("email").innerHTML =risultato;
+       }else if( mail == 'simone@gmail.com'){
+         document.getElementById("email").innerHTML =risultato;
 
-     }else if( mail == 'chiara@gmail.com'){
-        document.getElementById("email").innerHTML =risultato;
-     } else{
+       }else if( mail == 'chiara@gmail.com'){
+          document.getElementById("email").innerHTML =risultato;
+        } else{
         document.getElementById("email").innerHTML =negazione;
+      
     }
-}
 
+   }
+   // ho provato a mettere nome[i] al posto delle mail nell if ma non mi funziona
 
    
 
