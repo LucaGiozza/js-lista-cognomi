03593608cbba9880b1e3ac2a09e42f6c2771e3da
Luca Giozza <luca.giozza@gmail.com>
2021-05-31 16:36:44 +0200

    prova esercizio 2

---
+++ js-2/script.js
@@ -0,0 +1,18 @@
+// Lista Cognomi:
+// chiedi all’utente il cognome
+// inseriscilo in un array con altri cognomi:
+//  ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" (partendo da 1)
+//  della lista in cui il nuovo utente si trova
+
+var cognome = prompt('inserisci il tuo cognome');
+
+
+
+var lista = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+lista.push(cognome);
+lista.sort();
+
+document.getElementById("colori").innerHTML = lista;
+console.log('cognome');
--- js/script.js
+++ js/script.js
@@ -2,13 +2,7 @@
 // Chiedi all’utente la sua email,
 //  2.  controlla che sia nella lista di chi può accedere,
 //  3.  stampa un messaggio appropriato sull’esito del controllo.
-// Lista Cognomi:
-// chiedi all’utente il cognome
-// inseriscilo in un array con altri cognomi:
-//  ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
-// stampa la lista ordinata alfabeticamente
-// scrivi anche la posizione "umana" (partendo da 1)
-//  della lista in cui il nuovo utente si trova
+
 
  var mail = prompt('Inserisci la tua mail');
  var nomi = ['luca@gmail.com', 'matteo@gmail.com', 'simone@gmail.com', 'chiara@gmail.com' ];
