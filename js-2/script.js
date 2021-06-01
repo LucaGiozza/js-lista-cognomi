a91df514b9c2fa7b48750838005796e9c4413834
Luca Giozza <luca.giozza@gmail.com>
2021-06-01 16:24:21 +0200

    correzione esercizio cognomi

--- js-2/script.js
+++ js-2/script.js
@@ -6,43 +6,43 @@
 // scrivi anche la posizione "umana" (partendo da 1)
 //  della lista in cui il nuovo utente si trova
 
-var cognome = prompt('inserisci il tuo cognome');
+// var cognome = prompt('inserisci il tuo cognome');
 
 
 
-var lista = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
-lista.push(cognome);
+// var lista = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+// lista.push(cognome);
 
 
-document.getElementById("colori").innerHTML = lista;
-lista.sort();
+// document.getElementById("colori").innerHTML = lista;
+// lista.sort();
 
 
-// possibile soluzione
+//  possibile soluzione possibile
 
-//  var cognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+  var cognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
 
-//  var cognomeUtente = prompt('inserisci il cognome');
+  var cognomeUtente = prompt('inserisci il cognome');
 
-//  cognomi.push(cognomeUtente);
+  cognomi.push(cognomeUtente);
 
 
-// cognomi.sort();
-// console.log(cognomi);
+ cognomi.sort();
+ console.log(cognomi);
 
-// for(var i = 0; i < cognomi.length; i++){
-//     document.getElementById('colori').innerHTML += cognomi[i] + '';
-//      if ( cognomi[i] == cognomeUtente){
-//         document.getElementById('posizione-elemento').innerHTML = i + 1;
-//      }
-// }
+ for(var i = 0; i < cognomi.length; i++){
+     document.getElementById('colori').innerHTML += cognomi[i] + '';
+      if ( cognomi[i] == cognomeUtente){
+         document.getElementById('posizione-elemento').innerHTML = i + 1;
+      }
+ }
 
-// seconda soluzione nel caso in cui mi interessa solo la posizione
+//  seconda soluzione nel caso in cui mi interessa solo la posizione utilizzando while
 
-// var i = 0;
-// while(cognomeUtente != cognomi[i]){
-//     i++;
-// }
+//  var i = 0;
+//  while(cognomeUtente != cognomi[i]){
+//      i++;
+//  }
 
-// come mettere in ordine anche con lettere non tutte maiuscole:
-// cognomeUtente = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.substring(1).toLowerCase();
+//  come mettere in ordine anche con lettere non tutte maiuscole:
+//  cognomeUtente = cognomeUtente.charAt(0).toUpperCase() + cognomeUtente.substring(1).toLowerCase();
